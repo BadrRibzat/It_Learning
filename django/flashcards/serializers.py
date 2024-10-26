302e6107cfa88b51ec9e530451839fcdf747ce15
BadrRibzat <badrribzat@gmail.com>
2024-10-26 13:49:05 +0100

    Remove deleted files for cleaning cache

--- django/flashcards/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Flashcard
-
-class FlashcardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Flashcard
-        fields = ['id', 'word', 'question', 'correct_answer', 'level', 'lesson']
