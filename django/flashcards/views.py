7bda67a31dd324c38d9f3cc0e933bc0765449673
BadrRibzat <badrribzat@gmail.com>
2024-10-26 13:49:05 +0100

    Remove deleted files for cleaning cache

--- django/flashcards/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Flashcard
-from .serializers import FlashcardSerializer
-
-class FlashcardViewSet(viewsets.ModelViewSet):
-    queryset = Flashcard.objects.all()
-    serializer_class = FlashcardSerializer
