dc3cb0d57dc3ee89fe547d875976dac65e3f1b78
BadrRibzat <badrribzat@gmail.com>
2024-10-26 10:15:39 +0100

    Add .gitignore to exclude unnecessary files and directories

--- django/accounts/apps.py
+++ django/accounts/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 0b45ad5 (Add .gitignore to exclude unnecessary files and directories)
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
