df871640333a550bcb89752993958554f43e9f19
BadrRibzat <badrribzat@gmail.com>
2024-10-28 18:26:53 +0100

    Fix tailwindcss

--- frontend/src/views/layouts/MainLayout.vue
+++ frontend/src/views/layouts/MainLayout.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
+<!--
       <img class="mx-auto h-12 w-auto" src="@/assets/logo.png" alt="Logo" />
+-->
     </div>
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
