f2701d2a4736b0f961f8f3d284a74ad13f2a4db1
BadrRibzat <badrribzat@gmail.com>
2024-10-28 16:49:51 +0100

    Update chatbot/urls.py to include chatbot endpoint

--- django/chatbot/urls.py
+++ django/chatbot/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import ChatbotView
 
 urlpatterns = [
-
     path('chat/', ChatbotView.as_view(), name='chatbot'),
     path('chatbot/', ChatbotView.as_view(), name='chatbot'),
-
 ]
