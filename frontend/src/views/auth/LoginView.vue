1be42180a1bc84c08878ff6ecbd652d8fd05b470
BadrRibzat <badrribzat@gmail.com>
2024-10-30 17:00:58 +0100

    Fix: Add autocomplete attribute to password input

--- frontend/src/views/auth/LoginView.vue
+++ frontend/src/views/auth/LoginView.vue
@@ -26,6 +26,7 @@
               v-model="form.password"
               type="password"
               required
+   	      autocomplete="current-password"
               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
               :placeholder="$t('auth.password')"
             />
