41c2ed2fd323d52d7a5df563307825eba6c26291
BadrRibzat <badrribzat@gmail.com>
2024-10-31 18:11:01 +0100

    Add views for fetching user statistics and resetting user progress

--- django/accounts/views.py
+++ django/accounts/views.py
@@ -1,3 +1,4 @@
+# accounts/views.py
 from rest_framework import status, viewsets, permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
