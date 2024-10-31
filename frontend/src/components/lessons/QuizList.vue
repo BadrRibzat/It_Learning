ba9f4e5a93fc9d7c7e46563f9a1bc553627c791e
BadrRibzat <badrribzat@gmail.com>
2024-10-31 11:41:10 +0100

    created a new QuizList.vue component that displays a list of quizzes using the QuizCard component

---
+++ frontend/src/components/lessons/QuizList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-bold mb-4">Quizzes</h2>
+    <div v-if="quizzes.length === 0" class="text-gray-600">
+      No quizzes available for this lesson.
+    </div>
+    <div v-else class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <QuizCard v-for="quiz in quizzes" :key="quiz.id" :quiz="quiz" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import QuizCard from './QuizCard.vue';
+
+defineProps({
+  quizzes: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
