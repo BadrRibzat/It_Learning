66b0ce3a4540d506d431dcc8cfc1ba3d32dee53c
BadrRibzat <badrribzat@gmail.com>
2024-11-02 11:30:20 +0100

    Update the api services chat to send messages to the backend

--- frontend/src/api/services/chat.js
+++ frontend/src/api/services/chat.js
@@ -1,6 +1,6 @@
 import axios from '../axios';
 
 export const chatService = {
-  sendMessage: (message) => axios.post('/api/chat/', { message }),
+  sendMessage: (message) => axios.post('/api/chat/', { input: message }),
   getChatHistory: () => axios.get('/api/chat/history/'),
 };
