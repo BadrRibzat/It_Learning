d420cb35778e69c3346f12d8c87546239fb38d83
BadrRibzat <badrribzat@gmail.com>
2024-11-02 11:44:12 +0100

    Correct DJANGO_SETTINGS_MODULE in train_model.py

--- django/chatbot/train_model.py
+++ django/chatbot/train_model.py
@@ -2,7 +2,7 @@ import os
 import django
 
 # Set up Django environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Learn_English.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
 
 import spacy
