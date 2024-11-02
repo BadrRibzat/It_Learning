750aa19b97813053d56ec4e46499ea27daaff64c
BadrRibzat <badrribzat@gmail.com>
2024-11-02 19:55:37 +0100

    Update configuration to use environment variables properly for local and Docker setups

--- django/manage.py
+++ django/manage.py
@@ -2,7 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from decouple import config
 from dotenv import load_dotenv
 
 def main():
@@ -10,17 +9,11 @@ def main():
     # Load environment variables from .env file
     load_dotenv()
 
-    # Verify environment variables
-    print("DB_NAME:", os.getenv('DB_NAME'))
-    print("DB_USER:", os.getenv('DB_USER'))
-    print("DB_PASSWORD:", os.getenv('DB_PASSWORD'))
-    print("DB_HOST:", os.getenv('DB_HOST'))
-    print("DB_PORT:", os.getenv('DB_PORT'))
-
     if 'test' in sys.argv:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.test_settings')
     else:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
