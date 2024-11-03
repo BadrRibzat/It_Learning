cda54c527155f856f7c66dae1ee9fec0656f22fa
BadrRibzat <badrribzat@gmail.com>
2024-11-03 17:35:22 +0100

    Updated filenames and imports to follow multi-word convention

---
+++ frontend/src/views/ProfileView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="profile">
+    <h1>Profile</h1>
+    <p>Welcome to your profile!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileView',
+};
+</script>
