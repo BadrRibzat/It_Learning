7bbad1352705dddf720d0fbe40525a08fcb60862
BadrRibzat <117668755+BadrRibzat@users.noreply.github.com>
2024-11-03 16:18:03 +0100

    Delete frontend directory
    creating a new frontend structure

--- frontend/src/api/services/google-auth.js
+++
@@ -1,8 +0,0 @@
-import axios from '../axios';
-
-export const googleAuthService = {
-  initialize: () => axios.get('/api/auth/google/initialize/'),
-  callback: (code) => axios.post('/api/auth/google/callback/', { code }),
-  disconnect: () => axios.post('/api/auth/google/disconnect/'),
-  getStatus: () => axios.get('/api/auth/google/status/'),
-};
