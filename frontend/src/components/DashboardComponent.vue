0ef57b0597d6219775fdf5617d743e4aa22a9f8a
BadrRibzat <badrribzat@gmail.com>
2024-11-03 18:33:52 +0100

    Implemented logic for Dashboard, Header, Chatbot Interface, Footer, Sidebar, Loading, and Error components

--- frontend/src/components/DashboardComponent.vue
+++ frontend/src/components/DashboardComponent.vue
@@ -20,12 +20,20 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'DashboardComponent',
   computed: {
     ...mapGetters(['completedLessons', 'totalPoints', 'currentLevel']),
+  },
+  methods: {
+    ...mapActions(['fetchProfile', 'fetchProgress', 'fetchStatistics']),
+  },
+  async created() {
+    await this.fetchProfile();
+    await this.fetchProgress();
+    await this.fetchStatistics();
   },
 };
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import i18n from './i18n';
-import import './assets/css/tailwind.css';
+import './assets/css/tailwind.css';
 
 createApp(App)
   .use(i18n)
