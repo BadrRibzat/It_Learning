f41cad0962e0c9145bb96961a546c942fba856da
BadrRibzat <badrribzat@gmail.com>
2024-11-04 12:20:13 +0100

    Update views to integrate with Vuex store and API services

--- frontend/src/views/LessonDetailView.vue
+++ frontend/src/views/LessonDetailView.vue
@@ -1,12 +1,48 @@
 <template>
-  <div class="lesson-detail">
-    <h1>Lesson Detail</h1>
-    <p>Learn more about this lesson.</p>
+  <div class="lesson-detail-view">
+    <HeaderComponent />
+    <SidebarComponent />
+    <div v-if="currentLesson">
+      <h1>{{ currentLesson.title }}</h1>
+      <p>{{ currentLesson.content }}</p>
+    </div>
+    <div v-else>
+      <p>Loading lesson details...</p>
+    </div>
+    <FooterComponent />
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+import HeaderComponent from '../components/HeaderComponent.vue';
+import SidebarComponent from '../components/SidebarComponent.vue';
+import FooterComponent from '../components/FooterComponent.vue';
+
 export default {
   name: 'LessonDetailView',
+  components: {
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+  },
+  computed: {
+    ...mapGetters(['currentLesson']),
+  },
+  methods: {
+    ...mapActions(['fetchLesson']),
+  },
+  async created() {
+    const lessonId = this.$route.params.id;
+    await this.fetchLesson(lessonId);
+  },
 };
 </script>
+
+<style scoped>
+.lesson-detail-view {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
--- frontend/src/views/LevelsOverviewView.vue
+++ frontend/src/views/LevelsOverviewView.vue
@@ -1,12 +1,33 @@
 <template>
-  <div class="levels-overview">
-    <h1>Levels Overview</h1>
-    <p>Explore the different levels of learning English.</p>
+  <div class="levels-overview-view">
+    <HeaderComponent />
+    <SidebarComponent />
+    <LevelsComponent />
+    <FooterComponent />
   </div>
 </template>
 
 <script>
+import HeaderComponent from '../components/HeaderComponent.vue';
+import SidebarComponent from '../components/SidebarComponent.vue';
+import LevelsComponent from '../components/LevelsComponent.vue';
+import FooterComponent from '../components/FooterComponent.vue';
+
 export default {
   name: 'LevelsOverviewView',
+  components: {
+    HeaderComponent,
+    SidebarComponent,
+    LevelsComponent,
+    FooterComponent,
+  },
 };
 </script>
+
+<style scoped>
+.levels-overview-view {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
