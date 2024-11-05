aaf5dc515367c2f77bc5aed8d2eee06e4fc26420
BadrRibzat <badrribzat@gmail.com>
2024-11-05 15:42:58 +0100

    Fix TypeScript errors and ESLint warnings for i18n

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,26 +1,28 @@
 import { createI18n, I18n } from "vue-i18n";
-import en from "./locales/en.json";
-import ar from "./locales/ar.json";
-import fr from "./locales/fr.json";
-import de from "./locales/de.json";
-import es from "./locales/es.json";
-import ja from "./locales/ja.json";
-import ko from "./locales/ko.json";
-import zh from "./locales/zh.json";
+import { LocaleMessage } from "./types";
+
+import enUS from "./locales/en-US.json";
+import arSA from "./locales/ar-SA.json";
+import frFR from "./locales/fr-FR.json";
+import deDE from "./locales/de-DE.json";
+import esES from "./locales/es-ES.json";
+import jaJP from "./locales/ja-JP.json";
+import koKR from "./locales/ko-KR.json";
+import zhCN from "./locales/zh-CN.json";
 
 export default createI18n({
   legacy: false,
   locale: "en-US",
   fallbackLocale: "en-US",
   messages: {
-    "en-US": en,
-    "ar-SA": ar,
-    "fr-FR": fr,
-    "de-DE": de,
-    "es-ES": es,
-    "ja-JP": ja,
-    "ko-KR": ko,
-    "zh-CN": zh,
+    "en-US": enUS as LocaleMessage,
+    "ar-SA": arSA as LocaleMessage,
+    "fr-FR": frFR as LocaleMessage,
+    "de-DE": deDE as LocaleMessage,
+    "es-ES": esES as LocaleMessage,
+    "ja-JP": jaJP as LocaleMessage,
+    "ko-KR": koKR as LocaleMessage,
+    "zh-CN": zhCN as LocaleMessage,
   },
   numberFormats: {
     // Add number formats for each locale
