ad864426625bc251c50cdc55a65abe458165ceb5
BadrRibzat <badrribzat@gmail.com>
2024-11-07 16:25:03 +0100

    Update authService to include checkUser endpoint

--- flashcards-frontend/src/services/api/auth.js
+++ flashcards-frontend/src/services/api/auth.js
@@ -4,6 +4,7 @@ const authService = {
   login: (credentials) => axiosInstance.post('/login/', credentials),
   register: (userData) => axiosInstance.post('/register/', userData),
   logout: () => axiosInstance.post('/logout/'),
+  checkUser: (email, password) => axiosInstance.post('/check-user/', { email, password }),
 };
 
 export default authService;
