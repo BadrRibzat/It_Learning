162c2babe50ba593c74b0a3922e9756c77ccd72b
BadrRibzat <badrribzat@gmail.com>
2025-08-11 14:30:35 +0000

    Clean slate: removed old project files to restart with MERN stack

--- it-learn-frontend/src/components/stats/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as StatItem } from './StatItem.vue';
-export { default as StatCard } from './StatCard.vue';
-export { default as MetricItem } from './MetricItem.vue';
-export { default as ActivityHeatmap } from './ActivityHeatmap.vue';
--- it-learn/user_profile/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask_restx import Namespace
-from .routes import profile_ns
-
-__all__ = ['profile_ns']
